bac546a2ded42e761d2e1393d1a940f0b3e20254
erickssen00 <ericksilva000089@gmail.com>
2026-08-04 16:11:18 -0300

    revisao
    4 primeiros

--- inicio-revisao/1_variaveis.php
+++ inicio-revisao/1_variaveis.php
@@ -1,1 +1,10 @@
-<!-- 1ª Digitação (Aqui) -->
+<?php
+
+// Criação das variáveis
+$n1 = 5;
+$n2 = 16;
+
+$soma = $n1 + $n2;
+
+// Exibindo o resultado
+echo "A soma de n1 + n2 é igual a: $soma";
---
+++ inicio-revisao/2_decisao.php
@@ -0,0 +1,13 @@
+<?php
+
+//Variavel
+$nota = 7.5;
+
+// Estrutura de decisão (se/senão)
+if($nota >= 7) {
+    echo "Aprovado!";
+} elseif ($nota >= 5){
+    echo "Recuperação!";
+} else {
+    echo "Reprovado!";
+}
