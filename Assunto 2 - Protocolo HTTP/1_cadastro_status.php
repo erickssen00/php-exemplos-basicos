2fb922e090f33bf66251161252d71718ebfd951f
erickssen00 <ericksilva000089@gmail.com>
2026-08-18 16:11:54 -0300

    atualizacao
    Novo assunto

---
+++ Assunto 2 - Protocolo HTTP/1_cadastro_status.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    initial-scale=1.0">
+    <title>Cadastro - Status Codes</title>
+</head>
+<body>
+    <h1>Cadastro de Aluno (com Status Codes)</h1>
+    <form method="post" action="" >
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" require><br><br>
+
+        <label for="idade">Idade:</label>
+        <input type="text" name="idade" require><br><br>
+
+        <button type="submit">Enviar</button>
+    
+    </form>
+
+</body>
+
+</html>
